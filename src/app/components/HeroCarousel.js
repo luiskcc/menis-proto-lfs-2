d6386dace1ee8b0a4b672a9b217a1ddcefc6740e
Luis <114909465+luiskcc@users.noreply.github.com>
2025-09-08 18:30:43 +0200

    adjusted hero image size

--- src/app/components/HeroCarousel.js
+++ src/app/components/HeroCarousel.js
@@ -3,48 +3,56 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import Image from 'next/image';
-
 
 const slides = [
   {
     id: 1,
-    image:'/magma/hero/RH1539-23.jpg',
-    title: 'Slide 1',
+    image: '/magma/hero/RH1539-23.jpg',
+    title: 'Magma Arts & Congress',
   },
   {
     id: 2,
     image: '/tanque.png',
-    title: 'Slide 2',
+    title: 'Project Tanque',
   },
   {
     id: 3,
-    image: 'https://demoairbnbbucket.s3.us-east-2.amazonaws.com/section-1/santisimo_2.jpeg',
-    title: 'Slide 3',
+    image: '/magma/section-2/Fernando Alda_057.jpg',
+    title: 'Architectural Detail',
   },
   {
     id: 4,
-    image: 'https://demoairbnbbucket.s3.us-east-2.amazonaws.com/section-3/santisimo_35.jpg',
-    title: 'Slide 4',
+    image: '/magma/section-2/Hisao Suzuki_04.jpg',
+    title: 'Interior Space',  
   },
 ]
 
 export default function HeroCarousel() {
   return (
-    <div>
+    <div className="w-full">
       <Swiper
         modules={[Autoplay]}
         spaceBetween={0}
         slidesPerView={1}
-        speed={3000}
-        navigation
-        pagination={{ clickable: true }}
+        speed={1500}
         autoplay={{ delay: 6000, disableOnInteraction: true }}
-
+        className="w-full h-[calc(100vh-5rem)] md:h-[calc(100vh-6rem)]"
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
-            <Image src={slide.image} alt={slide.title} width={1000} height={1000} className="w-screen h-screen object-cover" />
+            <div className="relative w-full h-full">
+              <Image 
+                src={slide.image} 
+                alt={slide.title} 
+                fill
+                className="object-cover"
+                priority={slide.id === 1}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+              />
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
